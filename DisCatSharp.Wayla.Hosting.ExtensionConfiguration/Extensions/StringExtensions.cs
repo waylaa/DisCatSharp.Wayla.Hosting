8c13c04610bea2bd889e16c96a1114a740ab331e
Nikos <nikosnaoum127@gmail.com>
2023-08-21 22:59:05 +0300

    More summaries.

--- DisCatSharp.Wayla.Hosting.ExtensionConfiguration/Extensions/StringExtensions.cs
+++ DisCatSharp.Wayla.Hosting.ExtensionConfiguration/Extensions/StringExtensions.cs
@@ -4,6 +4,11 @@ namespace DisCatSharp.Wayla.Hosting.ExtensionConfiguration.Extensions;
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// Gets the namespace from the full name of an assembly.
+    /// </summary>
+    /// <param name="assemblyFullName">The full name of the assembly.</param>
+    /// <returns>[<see cref="string"/>] The namespace of the assembly.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string GetAssemblyNamespace(this string assemblyFullName)
         => assemblyFullName[..assemblyFullName.IndexOf(',')];
